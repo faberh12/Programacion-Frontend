ba8262e8c7ed62ae7c4fe48652700fffe916745e
faberh12 <hernandezfabian828@gmail.com>
2024-08-25 17:55:42 -0500

    Actualizacion Principal

--- components/Principal.tsx
+++ components/Principal.tsx
@@ -1,14 +1,24 @@
 import '../styles/Principal.css'
+import image1 from '../assets/image1.jpg'; // Asegúrate de que la ruta sea correcta
+import image2 from '../assets/image2.jpeg'; // Asegúrate de que la ruta sea correcta
 
 export default function Principal() {
-    return (
-        <div className='main'>
-        <div className='aside'>
-          this is the aside
-        </div>
-        <div className='main-principal'>
-          this is the principal content
-        </div>
+  return (
+      <div className='main'>
+          <div className='aside'>
+              This is the aside
+          </div>
+          <div className='main-principal'>
+              <img src={image1} alt="Description of Image 1" className="principal-image" />
+              <img src={image2} alt="Description of Image 2" className="principal-image" />
+              <article className="article">
+                  <h2>Article of politic</h2>
+                  <p>
+                      This is a short article about an interesting topic. 
+                      It contains a few sentences that provide some insights and information.
+                  </p>
+              </article>
+          </div>
       </div>
-    )
+  );
 }
